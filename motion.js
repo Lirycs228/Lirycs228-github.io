a4b3eee4f2f74b0566ab6f1d9038d53c194d3252
Lirycs228 <erik@hammon-mbe.de>
2017-01-31 17:18:08 +0100

    Update motion.js

--- motion.js
+++ motion.js
@@ -287,7 +287,7 @@ function next_day() {
 
 function help_first(num, char) {
   var buchs = num.split("");
-  if (1 in buchs) {
+  if (char == "") {
     document.getElementById(num).addEventListener("click", function(){  Suche = [];  Suche.push(char);  write(day_at_moment);  });
   } else {
     document.getElementById(num + char).addEventListener("click", function(){  Suche = [];  Suche.push(num, char);  write(day_at_moment);  });
