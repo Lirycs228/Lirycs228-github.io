20593cc91c8dcc19cb2831ec603e67c23496f7af
Lirycs228 <erik@hammon-mbe.de>
2017-02-03 16:04:16 +0100

    Update motion.js

--- motion.js
+++ motion.js
@@ -70,7 +70,6 @@ function writeData(nbr, lis) {
 };
 
 function indexer(index, nbr, lang, func, push, reset, re, set_lang, re_lang) {
-  alert("hallo");
   var e = firebase.database().ref('plan/' + "Tag_" + nbr.toString() + "/" + index.toString() + "/0");
   var f = firebase.database().ref('plan/' + "Tag_" + nbr.toString() + "/" + index.toString() + "/1");
   var g = firebase.database().ref('plan/' + "Tag_" + nbr.toString() + "/" + index.toString() + "/2");
@@ -96,13 +95,11 @@ function indexer(index, nbr, lang, func, push, reset, re, set_lang, re_lang) {
 };
 
 function handler(lang, func, push, reset, re, set_lang, re_lang) {
-  alert("hallo");
   set_lang(re_lang() + 1);
   if(re_lang() == lang){  set_lang(0);  func(re());  reset();  };
 };
 
 function readData(nbr, snapshot, func, push, reset, re, set_lang, re_lang) {
-  alert("hallo");
   var lang = snapshot.val();  reset();
   if(lang == 0){
     func(undefined);
