c2ed2d33b8551611557ee588f7f4803de95a471a
Lirycs228 <erik@hammon-mbe.de>
2017-02-04 10:30:40 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -41,7 +41,10 @@ function save_msg(msg, name) {
   firebase.database().ref("chat/msg/0").set(long + 1);
   firebase.database().ref("chat/msg/" + (long + 1) + "/head").set(name);
   firebase.database().ref("chat/msg/" + (long + 1) + "/body").set(msg);
-  load_msg(snapshot);
+  ref_zero.once("value", function(snapshot) {
+    alert("hi");
+    load_msg(snapshot);
+  });
 };
 function adder() {
   document.getElementById("send").addEventListener("click", function() {
