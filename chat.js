feb186692c5912d0dbbb24d646f698b6b2a29926
Lirycs228 <erik@hammon-mbe.de>
2017-02-04 18:45:35 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -53,9 +53,11 @@ function load_msg(index) {
     top.once("value", function(snapshot) {  top = snapshot.val();   }).then(
       mid.once("value", function(snapshot) {  mid = snapshot.val();
         msgs.push({head:top, body:mid});
+        if(i==index){
+          display_new_msg();
+        };
       }));
   };
-  display_new_msg();
 };
 
 function save_msg(msg, name) {
