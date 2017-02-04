9e40cb4f7bcb81dfb189e6ef9b31c639fc6cd99b
Lirycs228 <erik@hammon-mbe.de>
2017-02-04 11:35:14 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -12,6 +12,30 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var ref_zero = firebase.database().ref("chat/msg/0");
 
+function set_html(id, set) {
+  document.getElementById(id).innerHTML = set;
+};
+
+function call_change(id, func, color) {
+  document.getElementById(id).addEventListener(func, function () {  document.getElementById(id).style.backgroundColor=color;  });
+};
+
+function secure() {
+  var email = "";  var psw = "";  window.err = false;
+  set_html("e_pompt", "<div style='background-color:#444; z-index:3; width:180px; height:400px; position:absolute; top:20%; left:45%; color:#AAA;'> <button type='button' id='end' style='background-color:#FF0000; border:none; width:25px; height:20px;'>X</button> <center><br> E-Mail: <br> <input type='email' id='email' /> <br><br><br> Password: <br> <input type='password' id='psw' /><br><br><br> <button type='button'  id='login' style='border:none; background-color:#00FF00; width:100px; height:50px; font-size: 20px;'>Login</button> </center></div>");
+  call_change("end", "mouseover", "#AA0000");
+  call_change("end", "mouseout", "#FF0000");
+  call_change("login", "mouseover", "#00AA00");
+  call_change("login", "mouseout", "#00FF00");
+  document.getElementById("end").addEventListener("click", function () {  set_html("e_pompt", "");  window.location.replace('https://lirycs228.github.io');  });
+  document.getElementById("login").addEventListener("click", function () {
+    email = document.getElementById("email").value; psw = document.getElementById("psw").value;
+    firebase.auth().signInWithEmailAndPassword(email, psw).catch(function(error) {
+      window.err = true;  var errorCode = error.code;  var errorMessage = error.message;  alert(errorCode + "\n" + errorMessage);  window.location.replace('https://lirycs228.github.io');
+    })
+
+};
+
 function display_new_msg() {
   document.getElementById("msg_box").innerHTML = "";
   for (var i = 0; i < msgs.length; i++) {
