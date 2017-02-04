aba9b9cc319fa44bfff295dcac05857fd3c59d58
Lirycs228 <erik@hammon-mbe.de>
2017-02-04 11:22:15 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -14,21 +14,17 @@ var ref_zero = firebase.database().ref("chat/msg/0");
 
 function display_new_msg() {
   document.getElementById("msg_box").innerHTML = "";
-  alert("displaying msg...");
   for (var i = 0; i < msgs.length; i++) {
-    alert(msgs[i]);
     document.getElementById("msg_box").innerHTML =  "<div id='msg'><div id='msg_header'>" + msgs[i]["head"] + "</div><div id='msg_body'>" + msgs[i]["body"] + "</div></div>" + document.getElementById("msg_box").innerHTML;
   };
 };
 
 function load_msg(index) {
-  alert("loading..." + index);
   for (var i = 1; i <= index; i++) {
-    alert("load: " + i);
     var top = firebase.database().ref("chat/msg/" + i + "/head");
     var mid = firebase.database().ref("chat/msg/" + i + "/body");
-    top.once("value", function(snapshot) {  top = snapshot.val();   alert("got val of head");   }).then(
-      mid.once("value", function(snapshot) {  mid = snapshot.val();   alert("got val of body");
+    top.once("value", function(snapshot) {  top = snapshot.val();   }).then(
+      mid.once("value", function(snapshot) {  mid = snapshot.val();
         msgs.push({head:top, body:mid});
         alert("msgs pushed");
         display_new_msg();
@@ -55,6 +51,5 @@ function adder() {
 };
 
 ref_zero.on("value", function(snapshot) {
-  alert("hi");
   load_msg(snapshot.val());
 });
