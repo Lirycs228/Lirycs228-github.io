242c10947c4f02620055644b34ec6f2015065313
Lirycs228 <erik@hammon-mbe.de>
2017-02-05 12:39:20 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -65,6 +65,7 @@ function load_msg(index) {
   for (var i = 1; i <= index; i++) {
     var top = firebase.database().ref("chat/msg/" + i + "/head");
     var mid = firebase.database().ref("chat/msg/" + i + "/body");
+    alert(top);
     top.once("value", function(snapshot) {  top = snapshot.val();   }).then(
       mid.once("value", function(snapshot) {  mid = snapshot.val();
         msgs.push({head:top, body:mid});
