87fe987b140b7b67ecb285504e90d88379575ea7
Lirycs228 <erik@hammon-mbe.de>
2017-03-27 19:14:57 +0200

    Update main.js

--- wahl/main.js
+++ wahl/main.js
@@ -1,1 +1,48 @@
-//Erik
+alert("Häh");
+var fileName = "test.txt";
+var data = "";
+
+function getInfoByCode(c){
+  alert("get info");
+  if( data == "" ){
+    return 'DataNotReady' ;
+    alert("Data not ready")
+  } else {
+    alert("ready data")
+    var rx = new RegExp( "^(" + c + ")\\s+\\|\\s+(.+)\\s+\\|\\s+\\s+(.+)\\|", 'm' ) ;
+
+    var values = data.match(rx,'m');
+    return { a:values[2] , b:values[3] };
+  }
+};
+
+function readBody(xhr) {
+    var d;
+    if (!xhr.responseType || xhr.responseType === "text") {
+      alert("text");
+        d = xhr.responseText;
+    } else if (xhr.responseType === "document") {
+      alert("document");
+        d = xhr.responseXML;
+    } else {
+      alert("else uh oh");
+        d = xhr.response;
+    }
+    return d;
+}
+
+req = new XMLHttpRequest();
+req.onreadystatechange = function() {
+  alert("onreadystatechange");
+  alert("Loading" + req.readyState);
+  if (req.readyState == 4) {
+    alert("End");
+    alert(readBody(req));
+    data = readBody(req);
+    var ret = getInfoByCode("b");
+    alert(ret);
+  }
+};
+req.open("GET", fileName, true);
+alert("Start");
+req.send();
