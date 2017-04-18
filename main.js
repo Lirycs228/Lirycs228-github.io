ae8e53f32ae7e9dfc97ecf9b2ddde5916bfb2910
Lirycs228 <erik@hammon-mbe.de>
2017-04-18 19:07:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var folders = {"Oranien-Plan":"/plan/plan.htm",
-               "Wahlzettel"  :"/wahl/wahl.htm"};
+               "Wahlzettel"  :"/wahl/wahl.htm",
+               "Game"        :"/game/game.htm"};
 
 var keys = Object.keys(folders);
 var current;
@@ -9,6 +10,10 @@ function make_log() {
   for (i = 0; i < keys.length; i++) {
     element = document.getElementById("vesel");
     current = folders[keys[i]];
-    element.innerHTML = element.innerHTML + "<a href='" + current + "'><div class='main'><div class='header'>" + keys[i] + "</div><div class='body'>" + current + "</div></div></a>";
+    if(keys[i]=="Game") {
+      element.innerHTML = element.innerHTML + "<a href='" + current + "'><div class='main'><div class='header'>" + keys[i] + "</div><div class='body'>" + current + "</div><div class='out'>Coming soon</div></div></a>";
+    } else {
+      element.innerHTML = element.innerHTML + "<a href='" + current + "'><div class='main'><div class='header'>" + keys[i] + "</div><div class='body'>" + current + "</div></div></a>";
+    }
   }
 }
