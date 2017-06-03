35aae20cc997ba9aba930039d4ea522d89e94b3f
Lirycs228 <erik@hammon-mbe.de>
2017-06-03 10:37:05 +0200

    Update loged_in.php

--- wahl/loged_in.php
+++ wahl/loged_in.php
@@ -1,13 +1,20 @@
 <?php
 $name = $_GET['pass']
 
+echo '<script type="text/javascript" language="Javascript"> 
+alert("' + $name + '"); 
+</script> ';
+
+
 $f = fopen('test', 'rw');
 
 foreach($f as &$line) {
   $s = preg_split(";", $line);
   if($s[5]==$pass){
     $s = "done";
-    echo "done";
+    echo '<script type="text/javascript" language="Javascript"> 
+    alert("done"); 
+    </script> ';
   }
 }
 ?>
